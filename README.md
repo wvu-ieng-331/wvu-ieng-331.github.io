380627e8fc8ca42300b72b7469de7d05e0036dc9
Ozan Ozbeker <ozan.ozbeker@mail.wvu.edu>
2025-01-26 12:45:21 -0500

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wvu-ieng-331.github.io
+
 Course website for IENG 331: Computer Applications in Industrial Engineering
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from wvu-ieng-331-github-io!")
+
+
+if __name__ == "__main__":
+    main()
